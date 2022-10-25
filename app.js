a1d097af31557621ce8b143f855b4a3741895c07
Prateek Suman <prateeksuman51@gmail.com>
2022-10-25 23:45:59 +0530

    added style

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 const initialPrice = document.querySelector("#initial-price");
 const quantityOfStocks = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-btn");
+const msgOutput = document.querySelector("#output-msg");
+
+submitBtn.addEventListener("click", function clickListener() {
+  const ip = initialPrice.value;
+  const quantity = quantityOfStocks.value;
+  const curr = currentPrice.value;
+  calculateProfitAndLoss(ip, quantity, curr);
+});
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    const loss = (initial - current) * quantity;
+    const lossPercentage = (loss / initial) * 100;
+
+    showOutput(
+      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+    );
+  } else if (current > initial) {
+    const profit = (current - initial) * quantity;
+    const profitPercentage = (profit / initial) * 100;
+
+    showOutput(
+      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+    );
+  } else {
+    showOutput(`No pain no gain and no gain no pain`);
+  }
+}
+function showOutput(msg) {
+  msgOutput.innerHTML = msg;
+}
